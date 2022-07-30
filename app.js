babca7ea670b428fb489d096324892580e340efc
dan2443 <68905500+dan2443@users.noreply.github.com>
2022-07-30 21:06:42 +0300

    cors fix

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(bodyParser.json());
 
 //fixes CORS error
-/*app.use((req, res, next) => {
+app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
@@ -28,7 +28,6 @@ app.use(bodyParser.json());
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
-*/
 
 app.use("/auth", authRoutes);
 
